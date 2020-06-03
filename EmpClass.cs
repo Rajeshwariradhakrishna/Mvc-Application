479cc1b06f2a2c7379fc21e6f1df180af02bed79
Rajeshwariradhakrishna <66317986+Rajeshwariradhakrishna@users.noreply.github.com>
2020-06-03 10:07:14 +0530

    Add files via upload

---
+++ EmpClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Binding_Webgrid.Models
+{
+    public class EmpClass
+    {
+        public int EmployeeID { get; set; }
+        [DisplayName("Employee Name")]
+        public string EmployeeName { get; set; }
+        public int EmployeeAge { get; set; }
+        public int EmployeeSalary { get; set; }
+
+    }
+}
---
+++ EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Binding_Webgrid.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MVC_Binding_Webgrid.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        string conn = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+        // GET: Employee
+        public ActionResult Index()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection sqlconn = new SqlConnection(conn))
+            {
+                sqlconn.Open();
+                string sqlQuery = " select * from Employee";
+                SqlDataAdapter da = new SqlDataAdapter(sqlQuery, sqlconn);
+                da.Fill(dt);
+                return View(dt);
+            }
+        }
+
+        public ActionResult Create()
+        {
+
+            return View(new EmpClass());
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmpClass empclass)
+        {
+            using (SqlConnection sqlconn = new SqlConnection(conn))
+            {
+                sqlconn.Open();
+                string sqlQuery = "Insert into Employee values(@EmployeeID, @EmployeeName, @EmployeeAge, @EmployeeSalary)";
+                SqlCommand sqlcmd = new SqlCommand(sqlQuery, sqlconn);
+                sqlcmd.Parameters.Add(new SqlParameter() { ParameterName = "@EmployeeID", Value = empclass.EmployeeID });
+                sqlcmd.Parameters.Add(new SqlParameter() { ParameterName = "@EmployeeName", Value = empclass.EmployeeName });
+                sqlcmd.Parameters.Add(new SqlParameter() { ParameterName = "@EmployeeAge", Value = empclass.EmployeeAge});
+                sqlcmd.Parameters.Add(new SqlParameter() { ParameterName = "@EmployeeSalary", Value = empclass.EmployeeSalary });
+                sqlcmd.ExecuteNonQuery();
+               // sqlcmd.Parameters.AddWithValue(@EmployeeID, empclass.EmployeeID);
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
